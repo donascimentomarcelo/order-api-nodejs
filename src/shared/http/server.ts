cecd4c18cad22e249eea25c63b9e360210684f0b
Marcelo Sant'Anna <marcelojunin2010hotmail.com>
2022-12-02 18:24:46 -0300

    Creating route file

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log('first');
+import express from 'express';
+import cors from 'cors';
+import routes from '@shared/routes';
+
+const app = express();
+const port = 3333;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Server started on port ${port}!`));
