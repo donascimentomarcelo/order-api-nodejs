10d547d61aa28150fcbf53d7bae2884e7416eecb
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2022-12-06 12:44:44 -0300

    Using celebrate to validate requests

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import routes from '@shared/routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 const port = 3333;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.use(
     (
